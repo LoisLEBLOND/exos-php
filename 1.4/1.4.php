61d7114678ddf0c8110f082f59ba49811c0cc043
Loïs <lois.leblond2305@gmail.com>
2025-11-17 12:38:13 +0100

    exo 4

---
+++ 1.4/1.4.php
@@ -0,0 +1,44 @@
+<?php
+
+function pgcd_1($a, $b) {
+	$a = abs($a);
+	$b = abs($b);
+	while ($a != $b) {
+		if ($a > $b) {
+			$a = $a - $b;
+		} else {
+			$b = $b - $a;
+		}
+	}
+	return $a;
+    echo $a;
+}
+
+function pgcd_2($a, $b) {
+	$a = abs($a);
+	$b = abs($b);
+	while ($b != 0) {
+		$r = $a % $b;
+		$a = $b;
+		$b = $r;
+	}
+	return $a;
+    echo $a;
+}
+
+function pgcd_3($a, $b) {
+	$a = abs($a);
+	$b = abs($b);
+	if ($b == 0) {
+		return $a;
+	} else {
+		return pgcd_3($b, $a % $b);
+	}
+    echo $a;
+}
+
+pgcd_1(5, 8);
+
+pgcd_2(5, 8);
+
+pgcd_3(5, 8);
