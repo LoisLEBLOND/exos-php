17af2361e3964f7a7d0525ffd6136d262d80b21e
Loïs <lois.leblond2305@gmail.com>
2025-11-18 16:18:35 +0100

    exo 4.2

---
+++ 4.2/4.2.php
@@ -0,0 +1,26 @@
+<?php
+
+// Fonction qui reproduit str_contains sans utiliser de fonctions PHP (sauf strlen, count, isset)
+function my_str_contains($haystack, $needle) {
+	$haystack_len = strlen($haystack);
+	$needle_len = strlen($needle);
+	if ($needle_len === 0) {
+		return true;
+	}
+	if ($needle_len > $haystack_len) {
+		return false;
+	}
+	for ($i = 0; $i <= $haystack_len - $needle_len; $i++) {
+		$found = true;
+		for ($j = 0; $j < $needle_len; $j++) {
+			if (!isset($haystack[$i + $j]) || $haystack[$i + $j] !== $needle[$j]) {
+				$found = false;
+				break;
+			}
+		}
+		if ($found) {
+			return true;
+		}
+	}
+	return false;
+}
